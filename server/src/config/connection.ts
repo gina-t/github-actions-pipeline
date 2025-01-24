fcf89a5973ec234f0df1d5babaa18361fc106052
Tao <ginadrcoder@gmail.com>
2025-01-25 09:32:02 +1100

    update dependencies in server/package.json, create config.ts in server/src to handle environment configuration, add MongoDB Atlas connection string for production as a new repository secret in GitHub Actions

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,7 +1,5 @@
+import './config';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz';
 
@@ -17,13 +15,3 @@ const connectDB = async (): Promise<typeof mongoose.connection> => {
 };
 
 export default connectDB;
-
-
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// import mongoose from 'mongoose';
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
-
-// export default mongoose.connection;
