471514592399ab88a8119c8e1e427932f1108556
Tao <ginadrcoder@gmail.com>
2025-01-24 19:03:43 +1100

    modify root/package.json/scripts

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,3 +1,32 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Determine the environment and load the corresponding .env file
+const envFile = process.env.NODE_ENV === 'production' ? '../.env.production' : '../.env.development';
+dotenv.config({ path: path.resolve(__dirname, envFile) });
+import express from 'express';
+import connectDB from './config/connection.js';
+import routes from './routes/index.js';
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serves static files in the entire client's dist folder
+app.use(express.static(path.resolve(__dirname, '../../client/dist')));
+app.use(routes);
+const startServer = async () => {
+    try {
+        await connectDB(); // Ensure the database is connected
+        app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    }
+    catch (error) {
+        console.error('Failed to connect to the database', error);
+        process.exit(1);
+    }
+};
+startServer();
 // import express from 'express';
 // // import path from 'node:path';
 // import connectDB  from './config/connection.js';
@@ -12,24 +41,3 @@
 // db.once('open', () => {
 //   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 // });
-import express from 'express';
-import connectDB from './config/connection.js';
-import routes from './routes/index.js';
-const app = express();
-const PORT = process.env.PORT || 3001;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
-app.use(routes);
-const startServer = async () => {
-    try {
-        await connectDB(); // Ensure the database is connected
-        app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-    }
-    catch (error) {
-        console.error('Failed to connect to the database', error);
-        process.exit(1);
-    }
-};
-startServer();
