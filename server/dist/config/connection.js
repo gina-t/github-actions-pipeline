258b3e4876fb4c1079dece97062f838256e35edd
Tao <ginadrcoder@gmail.com>
2025-01-26 17:56:41 +1100

    move .env into root, modify server.ts, modify connection.ts, add step seed database in deploy-to-render.yml

--- server/dist/config/connection.js
+++ server/dist/config/connection.js
@@ -1,13 +1,11 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-import dotenv from 'dotenv';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
+// import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-// Resolve __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-// Load environment variables from the appropriate .env file
-const envFile = process.env.NODE_ENV === 'production' ? '../.env.production' : '../.env.development';
-dotenv.config({ path: path.resolve(__dirname, envFile) });
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const envFile = process.env.NODE_ENV === 'production' ? '../.env.production' : '../.env.development';
+// dotenv.config({ path: path.resolve(__dirname, envFile) });
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz';
 const connectDB = async () => {
     try {
--- server/dist/server.js
+++ server/dist/server.js
@@ -4,10 +4,8 @@ import path from 'path';
 import dotenv from 'dotenv';
 import connectDB from './config/connection.js';
 import routes from './routes/index.js';
-// Resolve __dirname and __filename for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// Load environment variables from the appropriate .env file
 const envFile = process.env.NODE_ENV === 'production' ? '../.env.production' : '../.env.development';
 dotenv.config({ path: path.resolve(__dirname, envFile) });
 const app = express();
